d727ffd1a4d2c9485a58d8f4c60b154b99c2e878
phan <bpa1911@mail.ru>
2023-03-15 22:12:25 +0300

    Initial Commit

---
+++ m16_git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m16_git
+//
+//  Created by Phan56 on 15.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
